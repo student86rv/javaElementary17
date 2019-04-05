6afdff0f1daecc4276e3a73fff33904b4ac3dfa6
Roman <student.rv86@gmail.com>
2019-04-05 14:38:45 +0300

    add functInterf count

--- testFile/src/com/company/Main.java
+++ testFile/src/com/company/Main.java
@@ -1,44 +1,67 @@
 package com.company;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
+    private static final String WORK_DIR = "D:/ElementaryLessons/javaElementary17/testFile/testSrc";
+    private static int javaCount = 0;
+    private static int funcCount = 0;
+
     public static void main(String[] args) {
 
-       // printFiles(System.getProperty("user.dir"));
+        // printFiles(System.getProperty("user.dir"));
 
-        int count = 0;
-        System.out.println("count:" + countJavaFiles("D:/ElementaryLessons/javaElementary17/testFile/testSrc", count));
+        System.out.println("Java files: " + countJavaFiles(WORK_DIR));
+        System.out.println("Functional interfaces: " + funcCount);
     }
 
-    private static void printFiles (String startDir) {
+    private static void printFiles(String startDir) {
         File homeDir = new File(startDir);
         if (homeDir.exists() && homeDir.isDirectory()) {
             File[] files = homeDir.listFiles();
-            for (File file: files) {
+            for (File file : files) {
                 System.out.println(file.getName());
                 printFiles(file.getAbsolutePath());
             }
         }
     }
 
-    private static int countJavaFiles(String startDir, int count) {
+    private static int countJavaFiles(String startDir) {
         File homeDir = new File(startDir);
         if (homeDir.exists() && homeDir.isDirectory()) {
             File[] files = homeDir.listFiles();
-            for (File file: files) {
+            for (File file : files) {
                 if (!file.isDirectory()) {
                     String[] nameAndExtension = file.getName().split("\\W+");
-                    System.out.println(nameAndExtension[1]);
-
                     if (nameAndExtension[1].equals("java")) {
-                        count++;
+                        javaCount++;
+                        if (fileReader(file.getAbsolutePath())) {
+                            funcCount++;
+                        }
                     }
                 }
-                countJavaFiles(file.getAbsolutePath(), count);
+                countJavaFiles(file.getAbsolutePath());
             }
         }
-        return count;
+        return javaCount;
+    }
+
+    private static boolean fileReader(String fileName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String currentLine;
+            while((currentLine = br.readLine()) != null) {
+               if (currentLine.equals("@FunctionalInterface")) {
+                   return true;
+               }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
